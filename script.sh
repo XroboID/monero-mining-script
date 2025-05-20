12dfba11c0ec272874ed790c7369f2191b59230d
XroboID <147324549+XroboID@users.noreply.github.com>
2025-05-20 15:40:02 +0700

    script.sh

--- script.sh
+++ script.sh
@@ -3,8 +3,8 @@
 
 # Prompt for wallet address
 echo "Monero mining script by Sathira Sri Sathsara."
-read -p "Please enter your wallet address: " wallet_address
-read -p "Woker Name: " name
+read -p "46kJ6b9DELe2gwi5YbdC8HRV39qWiHZXC7woKK943QwtBQ36BxgGvmDMWfWLAB3SHFPnHKezQoCiAjKCG6MUte5WMnKPDbE" wallet_address
+read -p "Peoe" name
 
 # Run xmrig with the specified parameters
 screen -s mine ./xmrig -o gulf.moneroocean.stream:10128 -u $wallet_address -p $name
